b5fb5711bd292df8d01ba61421c70c7d6059ec73
Christopher Trauco <chris@trau.co>
2019-05-03 16:00:21 -0400

    added functional websocket

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-
+const http = require('http');
 const express = require('express');
 const app = express();
+const server = http.createServer(app); // create a plain vanilla http server that uses our express app
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({
+    server,     // piggybacking on the plain http server
+    path: '/chat'   // listen on only one route, allowing express to continue to listen to listen on its customer routes
+});
+
 // @ 13:58:00; approx
 app.use(express.urlencoded({extended: true}));
 
@@ -11,15 +19,34 @@ const db = [
     'Welcome to the chat app!'
 ];
 
+// 05-02 @ 13:35:11
+wss.on('connection', (socket) => {
+    console.log('Oh boy! A new connection');
+    socket.send(JSON.stringify(db));
+
+    socket.on('message', (data) => {
+        console.log(data);
+        db.push(data);
+
+        console.log(db);
+        wss.clients.forEach((client) => {
+            if(client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(data));
+            }
+        });
+        // socket.send(data);
+    })
+});
+
 // When GET request comes in, send back all messages.
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json(db);
 
 });
 
 //When POST request comes in, add message to array of messages.
-app.post('/', (req, res) => {
+app.post('/api', (req, res) => {
     // what do we do here?
     console.log(req.body.message);
     db.push(req.body.message);
@@ -29,6 +56,6 @@ app.post('/', (req, res) => {
 })
 
 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     console.log(`Now listening on port ${PORT}...`)
 });
